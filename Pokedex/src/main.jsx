2bba2a9d47a81f78420ae12e0d443054cd821e69
plla2 <jhbljs92@naver.com>
2023-06-08 11:36:56 +0900

    change settings

--- Pokedex/src/main.jsx
+++ Pokedex/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools />
     <App />
-  </React.StrictMode>,
-)
+  </QueryClientProvider>
+);

8874517e123c428d7772025533603009c3c338cd
plla2 <jhbljs92@naver.com>
2023-06-11 03:10:00 +0900

    create IntroModal render

--- Pokedex/src/components/modal/IntroModal.jsx
+++ Pokedex/src/components/modal/IntroModal.jsx
@@ -1,4 +1,5 @@
 import { usePokemonModal } from "../../context/PokemonModalProvider";
+import { getTypeIconSrc } from "../../utils/helper";
 
 const IntroModal = () => {
   const { currentPokemon, closeModal } = usePokemonModal();
@@ -8,6 +9,21 @@ const IntroModal = () => {
       <a className="arrow-back" onClick={closeModal}></a>
       <div className="current-pokemon">
         <img src={currentPokemon.imgSrc} alt="Pokemon-Image" />
+        <div>
+          <span className="id-number"># {currentPokemon.paddedId}</span>
+          <span className="pokemon-name">{currentPokemon.name}</span>
+          <div className="types">
+            {currentPokemon.types.map(({ name }) => {
+              const typeImg = getTypeIconSrc(name);
+              return (
+                <div key={name} className={name}>
+                  <img src={typeImg} alt={name} />
+                  <span className="type-name">{name}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
